764a7516dab22d050c4250e99db21716a15f4529
Luis Castro <foreverluiscastro@gmail.com>
2025-01-06 22:18:49 -0500

    Updated sessions controller to also send profile picture when users login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
-      render json: @user
+      render json: user_with_picture(@user)
     else
       render json: { errors: ['Invalid username or password'] }, status: :unauthorized
     end
@@ -15,5 +15,12 @@ class Api::SessionsController < ApplicationController
   def destroy
     session.delete :user_id
     head :no_content
+  end
+
+  # Method to include the user's profile picture in the response
+  def user_with_picture(user)
+    user.as_json.merge(
+      profile_picture_url: user.profile_picture.attached? ? user.profile_picture.url : nil
+    )
   end
 end
